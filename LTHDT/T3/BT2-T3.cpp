ce10eb32b982ee2340b0f5cd556538cc839b1145
quycan-2003 <hsquy.dhti15a8hn@sv.uneti.edu.vn>
2022-04-05 22:24:13 +0700

    quyhs

--- LTHDT/T3/BT2-T3.cpp
+++ LTHDT/T3/BT2-T3.cpp
@@ -60,7 +60,7 @@ int main()
 		cout<<"--Moi nhap thong tin giao vien thu "<<i+1<<"--"<<endl;
 		cin>>a[i];
 	}
-	cout<<"-----------------------Thong tin gia vien------------------------"<<endl;
+	cout<<"-----------------------Thong tin giao vien-----------------------"<<endl;
 	cout<<"-----------------------------------------------------------------"<<endl;
 	for(int i=0; i<n; i++){
 		for(int j=i+1; j<n; j++){
