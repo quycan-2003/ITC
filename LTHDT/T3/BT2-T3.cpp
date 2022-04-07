7e979f692194fa53f022e63bdf55f9f4f8d1a03f
quycan-2003 <hsquy.dhti15a8hn@sv.uneti.edu.vn>
2022-04-07 15:11:41 +0700

    quyhs

--- LTHDT/T3/BT2-T3.cpp
+++ LTHDT/T3/BT2-T3.cpp
@@ -18,7 +18,7 @@ class GV{
 			cout<<"Tuoi: ";
 			is>>a.t;
 			cin.ignore();
-			cout<<"Bang cap cao nhat: ";
+			cout<<"Bang cap cap nhat: ";
 			is.getline(a.bc, 15);
 			cout<<"Chuyen nganh: ";
 			is.getline(a.cn, 20);
